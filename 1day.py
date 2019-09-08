66f01247bfcc14cbbd8d1491fbb03e6246b26a87
hexelulu <yululu89@hotmail.com>
2019-09-08 19:04:22 +0200

    Day1_excercise_lulu Ver.1 08/09/2019

---
+++ 1day.py
@@ -0,0 +1,14 @@
+myteacher = "小甲鱼"
+yourteacher = myteacher
+yourteacher = "黑夜"
+print(myteacher)
+
+myteacher = "小甲鱼"
+yourteacher = myteacher
+myteacher ="黑夜"
+print(yourteacher)
+
+first = 520
+second = "520"
+first = second
+print(first)
